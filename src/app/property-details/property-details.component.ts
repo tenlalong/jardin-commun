3d358dbcf8f55abaf470f4789b8fb229d800800b
tenlalong <long26800@gmail.com>
2025-07-31 15:26:31 -0400

    Fix side bar issue

---
+++ src/app/property-details/property-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-details',
+  imports: [],
+  templateUrl: './property-details.component.html',
+  styleUrl: './property-details.component.css'
+})
+export class PropertyDetailsComponent {
+
+}
